745a14c2b2058811c245bfaa4352f1f1377af5b9
arbaazshaikh08 <arbajshaikh@gmail.com>
2024-10-09 08:19:39 +0530

    bugs clear complated

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -4,6 +4,23 @@ import { User} from "../models/user.model.js"
 import {uploadOnCloudinary} from "../utils/cloudinary.js"
 import { ApiResponse } from "../utils/ApiResponse.js";
 
+const generateAccessAndRefereshTokens = async(userId) =>{
+    try {
+         const user = await User.findById(userId)
+        const accessTokan = user.generateAccessTokens()
+        const refereshToken = user.generaterefreshToken()
+
+        user.refereshToken = refereshToken
+        await user.save({ validateBeforeSave: false })
+
+        return {accessToken, refreshToken}
+
+
+    } catch (error) {
+        throw new ApiError(500, "Something went wrong while generating referesh and access token")
+    }
+}
+
 const registerUser = asyncHandler( async (req, res) => {
     // get user details from frontend
     // validation - not empty
@@ -76,9 +93,68 @@ const registerUser = asyncHandler( async (req, res) => {
         new ApiResponse(200, createdUser, "User registered Successfully")
     )
 
-
 } )
+
+const loginUser = asyncHandler(async (req, res) =>{
+    // req body -> data
+    // username or email
+    //find the user
+    //password check
+    //access and referesh token
+    //send cookie
+
+    const {email, username, password} = req.body
+    console.log(email);
+
+    if (!username && !email) {
+        throw new ApiError(400, "username or email is required")
+    }
+    const user = await User.findOne({
+        $or: [{username},{email}]
+    })
+
+   if (!user){
+    throw new ApiError(404,"user does not exist")
+   }
+
+   const isPasswordValid = await user.isPasswordCorrect(password)
+
+    if (!isPasswordValid) {
+     throw new ApiError(401, "Invalid user credentials")
+     }
+
+     const {accessTokan, refereshToken} = await generateAccessAndRefereshTokens(user._id)
+
+    const loggedInUser = await User.findById(user._id).select("-password -refreshToken")
+
+    const options = {
+        httpOnly: true,
+        secure: true
+    }
+
+    return res
+    .status(200)
+    .cookie("accessToken", accessToken, options)
+    .cookie("refereshToken",refreshToken,options)
+    .json(
+        new ApiResponse(
+            200, 
+            {
+                user: loggedInUser, accessToken, refreshToken
+            },
+            "User logged in Succesfully"
+        )
+    )
+
+})
+
+const logoutUser = asyncHandler(async(req,res)=>{
+   
+})
+
 
 export {
     registerUser,
+    loginUser,
+    logoutUser
 }
--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -11,5 +11,5 @@
   })
   
 export const upload = multer({ 
-    storage, 
+    storage 
 }) 
